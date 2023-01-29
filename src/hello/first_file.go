1abbf6dd575b9e63fa6592e77562ffce92d153c6
subhash99 <snarkedamilli@rijyos.com>
2023-01-29 14:46:47 +0530

    Learn Go Language

--- src/hello/first_file.go
+++ src/hello/first_file.go
@@ -152,9 +152,105 @@ func main() {
 	// 1.Creation
 	// 2.Built In Functions
 	// 3.Working with Arrays
+	// 4. make operation is used to create an array
+
+	grades := [3]int{12, 13, 14}
+
+	fmt.Println(grades[1])
+
+	fmt.Printf(" %v %T\n", grades[1], grades)
+
+	var students [3]string
+
+	students[0] = "student1"
+	students[1] = "student2"
+	students[2] = "student3"
+
+	fmt.Println("length of the array", students, len(students))
+	fmt.Println("capacity of the array", students, cap(students))
+
+	// 3 x 3 matrix
+
+	var twoDimentionalMatrix [3][3]string
+
+	twoDimentionalMatrix[0] = [3]string{"firstRow1", "firstRow2", "firstRow3"}
+	twoDimentionalMatrix[1] = [3]string{"secoundRow1", "secoundRow2", "secoundRow3"}
+	twoDimentionalMatrix[2] = [3]string{"thirdRow1", "thirdRow2", "thirdRow3"}
+
+	fmt.Printf("%v, %T", twoDimentionalMatrix, twoDimentionalMatrix)
+
+	// copying arrays and new reference is created
+
+	array1 := [...]int{1, 2, 3}
+	array2 := array1
+	array2[1] = 5
+	fmt.Println("\n", array1, array2)
+
+	array3 := &array1
+
+	array3[1] = 100001
+
+	fmt.Println(array1, array3)
+
+	make1 := make([]int, 10)
+	make2 := make([]int, 10, 100) // capacoty = 100
+	fmt.Println("Using make Funcion creating the array", make1)
+	fmt.Printf("Using make %v, and Capacity = %v", len(make2), cap(make2))
+
 	// Slices
 	// 1.Creation
 	// 2.Built In Functions
 	// 3.Working with Slices
 
+	slice1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	slice2 := slice1
+
+	fmt.Println("before Changing the value", slice1, slice2)
+
+	slice2[1] = 33333
+
+	fmt.Println("After changing the value", slice1, slice2)
+
+	slice3 := slice1[:]
+	slice4 := slice1[:3]
+	slice5 := slice1[3:]
+	slice6 := slice1[3:7]
+	fmt.Println("slice all elements", slice3)
+	fmt.Println("slice upto index 3", slice4)
+	fmt.Println("slice from index 3 to end", slice5)
+	fmt.Println("slice from index 3 to 6", slice6)
+
+	// NOTE :: Slicing operations also works with arrays
+
+	array4 := array1[1:2]
+	fmt.Println(array4)
+
+	// when we append the new elements to the array, then the capacity of the array increases
+	// if length crosses the capacity, then the capaciuty will be incremented by 2 times
+	// Example :: If length is 7 and capacity is 8. New element(9) is added.
+	// 9 ---> add operation --> len = 8, cap = 8
+	// 10 ---> add operation --> len = 9, cap = 8 * 2 = 16
+	// upto length 16, it accepts all add opertioans. When length crosses the 16 the capacity is multiplied by 2
+	// 16 --> len = 17, cap = 32
+
+	cap1 := []int{}
+	fmt.Println(cap1, len(cap1), cap(cap1))
+	cap1 = append(cap1, 2, 3, 4)
+	fmt.Println(cap1, len(cap1), cap(cap1))
+	cap1 = append(cap1, 2, 3, 4, 5)
+	fmt.Println(cap1, len(cap1), cap(cap1))
+	cap1 = append(cap1, 2, 3, 4, 5)
+	fmt.Println(cap1, len(cap1), cap(cap1))
+	cap1 = append(cap1, 2, 3, 4, 5, 7, 8)
+	fmt.Println(cap1, len(cap1), cap(cap1))
+	cap2 := []int{1, 2, 3}
+	cap2 = append(cap2, []int{4, 5, 6}...)
+	fmt.Println("append operation output using three dots", cap2, len(cap2))
+
+	// Remove operations in slices
+	deleteOp := []int{2, 3, 4, 5, 6}
+	fmt.Println(deleteOp)
+	deleteOp1 := append(deleteOp[:1], deleteOp[2:]...)
+	fmt.Println("delete the secound element", deleteOp1)
 }
